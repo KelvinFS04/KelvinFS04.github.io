4b8154b8cee07bed6469e21a21e6493da7b81e80
KelvinFS04 <kelvin_fs04@hotmail.com>
2022-10-01 15:46:06 -0300

    Mais operações adicionadas

--- Páginas/Projetos/Calculadora/Calculadora.js
+++ Páginas/Projetos/Calculadora/Calculadora.js
@@ -1,23 +1,42 @@
 
         function númeroA(){
             númeroDigitadoA = document.getElementById("input_A").value;
+            númeroDigitadoA = parseInt(númeroDigitadoA);
             console.log(númeroDigitadoA);
-            númeroDigitadoA = parseInt(númeroDigitadoA)
-
             return númeroDigitadoA;
 
         }
         function númeroB(){
             númeroDigitadoB = document.getElementById("input_B").value;
-            console.log(númeroDigitadoB);
             númeroDigitadoB = parseInt(númeroDigitadoB);
+            console.log(númeroDigitadoB);
             return númeroDigitadoB;
 
         }
 
+
+
         function soma(){
-            var soma = númeroA() + númeroB() ;
-            console.log(soma);
-            document.getElementById('saída').innerHTML = soma;
+            var soma = númeroA() + númeroB();
+            if (soma != NaN) {
+                
+                document.getElementById('saída').innerHTML = soma;
+                
+            } else {
+                document.getElementById('saída').innerHTML = "Digite um valor válido";
+            }
+            console.log(soma)
+        }
+        function subtração(){
+            var subtração = númeroA() - númeroB();
+            document.getElementById('saída').innerHTML = subtração;
+        }
+        function multiplicação(){
+            var multiplicação = númeroA() * númeroB();
+            document.getElementById('saída').innerHTML = multiplicação;
+        }
+        function divisão(){
+            var divisão = númeroA() / númeroB();
+            document.getElementById('saída').innerHTML = divisão;
         }
     
