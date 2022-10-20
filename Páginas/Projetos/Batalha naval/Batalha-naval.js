fcca53d55ea59e2946a0cf7b23f68b831b9a71cf
KelvinFS04 <kelvin_fs04@hotmail.com>
2022-10-20 16:42:39 -0300

    Teste de mudanças nas matrizes

--- Páginas/Projetos/Batalha naval/Batalha-naval.js
+++ Páginas/Projetos/Batalha naval/Batalha-naval.js
@@ -1,13 +1,38 @@
+let matriz = [,];
+for (let a = 0; a < 5; a++) 
+{
+    for (let b = 0; b < 5; b++) 
+    {
+        matriz[a,b] = "" + a + b;
+    }
+}
+
+function mudaMapa()
+{
+    for (let c = 0; c < 5; c++) 
+    {
+        for (let d = 0; d < 5; d++) 
+        {
+            let id = "x" + c + "y" + d;
+            document.getElementById(id).innerHTML = "a";
+        } 
+    }
+}
+
 function geraMapa()
 {
-Tabela = ""
+    Tabela = "";
     for (let i = 0; i < 5; i++){
-        Tabela += "<tr>"
+        Tabela += "<tr>";
         for (let j = 0; j < 5; j++){
             Tabela += '<td id = "x' + j;
-            Tabela += 'y' + i + '"> teste </td>'
+            Tabela += 'y' + i + '"> teste </td>';
         }
-        Tabela += "</tr>"
+        Tabela += "</tr>";
     }
-    document.getElementById("mapa").innerHTML = Tabela
+    document.getElementById("mapa").innerHTML = Tabela;
 }
+
+
+
+
