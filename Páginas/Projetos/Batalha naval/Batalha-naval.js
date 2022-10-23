0f9b842aef2a71a89941558664d563f385f2d39e
KelvinFS04 <kelvin_fs04@hotmail.com>
2022-10-23 16:29:03 -0300

    Cruzadores e Porta Aviões Adicionados

--- Páginas/Projetos/Batalha naval/Batalha-naval.js
+++ Páginas/Projetos/Batalha naval/Batalha-naval.js
@@ -1,13 +1,3 @@
-let matriz = [,];
-for (let a = 0; a < 8; a++) 
-{
-    for (let b = 0; b < 8; b++) 
-    {
-        matriz[a,b] = "" + a + b;
-    }
-}
-
-
 function geraMapa()
 {
     Tabela = "";
@@ -22,13 +12,15 @@ function geraMapa()
     document.getElementById("mapa").innerHTML = Tabela;
     adicionaSubmarinos();
     adicionaTorpedeiro();
+    adicionaCruzador();
+    adicionaPortaAviões();
 
 }
 
 function adicionaSubmarinos()
 {
     
-    for (let i = 0; i <= 5; i++) 
+    for (let i = 0; i < 5; i++) 
     {
        
         id = "x" + numRandômico(0, 7) + "y" + numRandômico(0, 7);
@@ -46,7 +38,7 @@ function adicionaSubmarinos()
 function adicionaTorpedeiro()
 {
     quantidadeDeBarcos = 0
-    while(quantidadeDeBarcos <=4)
+    while(quantidadeDeBarcos <4)
     {
         let orientação = numRandômico(1,2);
         console.log(orientação)
@@ -124,6 +116,174 @@ function adicionaTorpedeiro()
         }
         quantidadeDeBarcos++
     }  
+}
+
+function adicionaCruzador()
+{
+    quantidadeDeBarcos = 0
+    while(quantidadeDeBarcos <3)
+    {
+        let orientação = numRandômico(1,2);
+        console.log(orientação)
+        if(orientação ==1)
+        {
+            do 
+            {
+                x = numRandômico(0, 7); 
+                y = numRandômico(0, 5);
+                id = "x" + x + "y" + y;
+            
+                deuCerto = true
+                if(document.getElementById(id).outerText == "Água")
+                { 
+                    for (let j = 0; j < 3; j++) 
+                    {
+                        var auxiliarY = y + j 
+                        var idAUX = "x" + x + "y" + auxiliarY;
+                        if(document.getElementById(idAUX).outerText != "Água")
+                        {
+                            deuCerto = false
+                        }
+                    }
+                }
+                else
+                {
+                    deuCerto = false
+                }
+                
+            } while (deuCerto == false );   
+            
+            
+            for (let j = 0; j < 3; j++) 
+            {
+                var auxiliarY = y + j
+                var idAUX = "x" + x + "y" + auxiliarY;
+                document.getElementById(idAUX).innerHTML = "C";
+            }
+        }
+        else
+        {
+            do 
+            {
+                x = numRandômico(0, 5); 
+                y = numRandômico(0, 7);
+                id = "x" + x + "y" + y;
+            
+                deuCerto = true
+                if(document.getElementById(id).outerText == "Água")
+                { 
+                    for (let j = 0; j < 3; j++) 
+                    {
+                        var auxiliarX = x + j 
+                        var idAUX = "x" + auxiliarX + "y" + y;
+                        if(document.getElementById(idAUX).outerText != "Água")
+                        {
+                            deuCerto = false
+                        }
+                    }
+                }
+                else
+                {
+                    deuCerto = false
+                }
+                
+            } while (deuCerto == false );   
+            
+            
+            for (let j = 0; j < 3; j++) 
+            {
+                var auxiliarX = x + j
+                var idAUX = "x" + auxiliarX + "y" + y;
+                document.getElementById(idAUX).innerHTML = "C";
+            }
+        }
+        quantidadeDeBarcos++
+    }  
+
+}
+
+function adicionaPortaAviões()
+{
+    quantidadeDeBarcos = 0
+    while(quantidadeDeBarcos <3)
+    {
+        let orientação = numRandômico(1,2);
+        console.log(orientação)
+        if(orientação ==1)
+        {
+            do 
+            {
+                x = numRandômico(0, 7); 
+                y = numRandômico(0, 4);
+                id = "x" + x + "y" + y;
+            
+                deuCerto = true
+                if(document.getElementById(id).outerText == "Água")
+                { 
+                    for (let j = 0; j < 4; j++) 
+                    {
+                        var auxiliarY = y + j 
+                        var idAUX = "x" + x + "y" + auxiliarY;
+                        if(document.getElementById(idAUX).outerText != "Água")
+                        {
+                            deuCerto = false
+                        }
+                    }
+                }
+                else
+                {
+                    deuCerto = false
+                }
+                
+            } while (deuCerto == false );   
+            
+            
+            for (let j = 0; j < 4; j++) 
+            {
+                var auxiliarY = y + j
+                var idAUX = "x" + x + "y" + auxiliarY;
+                document.getElementById(idAUX).innerHTML = "PA";
+            }
+        }
+        else
+        {
+            do 
+            {
+                x = numRandômico(0, 4); 
+                y = numRandômico(0, 7);
+                id = "x" + x + "y" + y;
+            
+                deuCerto = true
+                if(document.getElementById(id).outerText == "Água")
+                { 
+                    for (let j = 0; j < 4; j++) 
+                    {
+                        var auxiliarX = x + j 
+                        var idAUX = "x" + auxiliarX + "y" + y;
+                        if(document.getElementById(idAUX).outerText != "Água")
+                        {
+                            deuCerto = false
+                        }
+                    }
+                }
+                else
+                {
+                    deuCerto = false
+                }
+                
+            } while (deuCerto == false );   
+            
+            
+            for (let j = 0; j < 4; j++) 
+            {
+                var auxiliarX = x + j
+                var idAUX = "x" + auxiliarX + "y" + y;
+                document.getElementById(idAUX).innerHTML = "PA";
+            }
+        }
+        quantidadeDeBarcos++
+    }  
+
 }
 
 function numRandômico(min, max)
