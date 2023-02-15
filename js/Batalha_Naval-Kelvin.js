ce120b928234f300660c8ad1c10cf45efe6e9112
KelvinFS04 <kelvin_fs04@hotmail.com>
2023-02-15 12:45:47 -0300

    Submarino add

---
+++ js/Batalha_Naval-Kelvin.js
@@ -0,0 +1,21 @@
+//declarando o mapa
+const tamanhoMapa = 10;
+let mapa = new Array();
+for (let i = 0; i < tamanhoMapa; i++) {
+    coluna = new Array();
+    mapa.push(coluna);
+        for(let j = 0; j < tamanhoMapa; j++) {
+            coluna.push("A")
+        }
+}
+
+//adicionando embarcações
+function embarcaçõesBot() 
+{
+    submarinoBot()    
+}
+
+function submarinoBot()
+{
+    
+}
--- js/Batalha_Naval.js
+++ js/Batalha_Naval.js
@@ -1,15 +1,43 @@
+
+
 //declarando o mapa
+const tamanhoMapa = 10;
 let mapa = new Array();
-for (let i = 0; i < 10; i++) {
-    coluna = new Array();
-    mapa.push(coluna);
-        for(let j = 0; j < 10; j++) {
-            coluna.push("A")
+for (let i = 0; i < tamanhoMapa; i++) {
+    vetor = new Array();
+    mapa.push(vetor);
+        for(let j = 0; j < tamanhoMapa; j++) {
+            vetor.push("A");
         }
 }
 
-//adicionando embarcações
-function adicionaEmbarcações() 
+//embarcações bot
+const quantidadeSubmarinos = 5;
+function embarcaçõesBot() 
 {
-    
+    submarinoBot();  
+}
+
+function submarinoBot()
+{
+    for (let i = 0; i < quantidadeSubmarinos; i++) {
+        let x = random(0, tamanhoMapa);
+        let y = random(0, tamanhoMapa);
+
+        if (mapa[x][y] == "A") {
+            mapa[x][y] = "S";
+        }
+        else
+        {
+            i--;
+        }
+    }
+}
+
+function random(min, max)
+{
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min);
+    //valor min incluso, mas max não
 }
